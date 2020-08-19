efbc585cdf10f3e0817b2cac1fb9567d76eccf9f
Dhruv Patel <dhruv.patel1@yahoo.com>
2020-08-19 02:53:48 -0400

    Updates

---
+++ precache-manifest.a9fa3e7c7414a6d16c8cc825e11a4b3c.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f221b914db6cd35a8d2cc66adbe71d6",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "46f61aba1959e889a052",
+    "url": "/portfolio-v2/static/css/main.c5eacc58.chunk.css"
+  },
+  {
+    "revision": "30227892cfa2b955340f",
+    "url": "/portfolio-v2/static/js/2.c6a29554.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-v2/static/js/2.c6a29554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f61aba1959e889a052",
+    "url": "/portfolio-v2/static/js/main.07e686ba.chunk.js"
+  },
+  {
+    "revision": "962abfe3789ceee4fbb2",
+    "url": "/portfolio-v2/static/js/runtime-main.3514a9a5.js"
+  },
+  {
+    "revision": "9215d470c4a74e1938d4a65441d794f9",
+    "url": "/portfolio-v2/static/media/about.9215d470.svg"
+  },
+  {
+    "revision": "c2d5c0596992f26b04dc5e3e0e38100d",
+    "url": "/portfolio-v2/static/media/anime.c2d5c059.jpg"
+  },
+  {
+    "revision": "08161b5e784179be188a7c9ac49dfe2b",
+    "url": "/portfolio-v2/static/media/contact-svg.08161b5e.svg"
+  },
+  {
+    "revision": "f12b0da588ea7f4df09656a8a80d64ed",
+    "url": "/portfolio-v2/static/media/contact.f12b0da5.svg"
+  },
+  {
+    "revision": "cb113e03d9321c079595deeb4fc57d2f",
+    "url": "/portfolio-v2/static/media/css3.cb113e03.svg"
+  },
+  {
+    "revision": "c2532d831291fd3a5033084cdd980ef9",
+    "url": "/portfolio-v2/static/media/cv.c2532d83.svg"
+  },
+  {
+    "revision": "783040f8e347608c4aba93bde8510017",
+    "url": "/portfolio-v2/static/media/design.783040f8.svg"
+  },
+  {
+    "revision": "c33a049436126c6f2aef3288745a0854",
+    "url": "/portfolio-v2/static/media/development.c33a0494.svg"
+  },
+  {
+    "revision": "c9bcda5f25ca076ce711a71d3af7ba3c",
+    "url": "/portfolio-v2/static/media/expensify.c9bcda5f.PNG"
+  },
+  {
+    "revision": "e772cb169188e9fe3f1380f458562478",
+    "url": "/portfolio-v2/static/media/fitness.e772cb16.jpg"
+  },
+  {
+    "revision": "1e84ee87170972393cf6b10b388820f8",
+    "url": "/portfolio-v2/static/media/gaming.1e84ee87.jpg"
+  },
+  {
+    "revision": "0efb93f1b60a59dbab97179c65779c80",
+    "url": "/portfolio-v2/static/media/git-logo.0efb93f1.svg"
+  },
+  {
+    "revision": "99b7e64bf5e9f3aacf71a34f30b6504f",
+    "url": "/portfolio-v2/static/media/hiking.99b7e64b.jpg"
+  },
+  {
+    "revision": "79eaf5c107a2055e47262f2f88a71e5b",
+    "url": "/portfolio-v2/static/media/html5.79eaf5c1.svg"
+  },
+  {
+    "revision": "f74d1650f89196dc0a903eaf7b0e5bab",
+    "url": "/portfolio-v2/static/media/java.f74d1650.svg"
+  },
+  {
+    "revision": "7bd7d08a9db3806433eb2ebf41e9fbfe",
+    "url": "/portfolio-v2/static/media/js.7bd7d08a.svg"
+  },
+  {
+    "revision": "a45907648b0855af6530c8befcce1f81",
+    "url": "/portfolio-v2/static/media/l-in.a4590764.svg"
+  },
+  {
+    "revision": "24bddb6ccffbc8923e60e0c7b8e67ee5",
+    "url": "/portfolio-v2/static/media/me.24bddb6c.jpg"
+  },
+  {
+    "revision": "e3f5888798062e5618d8d3cbf76d8c84",
+    "url": "/portfolio-v2/static/media/meal-scouter.e3f58887.PNG"
+  },
+  {
+    "revision": "8d7858a0b1856b47851bc0ed0739c89b",
+    "url": "/portfolio-v2/static/media/movies.8d7858a0.jpg"
+  },
+  {
+    "revision": "98af28b7e7910f1fc4965e8539f4cd95",
+    "url": "/portfolio-v2/static/media/node.98af28b7.svg"
+  },
+  {
+    "revision": "b1073ac39f833d6be443c2ef21bd957c",
+    "url": "/portfolio-v2/static/media/portfolio.b1073ac3.PNG"
+  },
+  {
+    "revision": "70d58fed0dc309eaa8b714467aee2a84",
+    "url": "/portfolio-v2/static/media/project-button.70d58fed.svg"
+  },
+  {
+    "revision": "126e5348d1c5686b0d2c279c4b57f436",
+    "url": "/portfolio-v2/static/media/projects.126e5348.svg"
+  },
+  {
+    "revision": "e3fe5ccd5ae28ccbf57205f3f7aacd57",
+    "url": "/portfolio-v2/static/media/python.e3fe5ccd.svg"
+  },
+  {
+    "revision": "9c757af563cc28d9b660ff6b41157c93",
+    "url": "/portfolio-v2/static/media/random-vid.9c757af5.PNG"
+  },
+  {
+    "revision": "6ca9d7367f92916778472db30bb90342",
+    "url": "/portfolio-v2/static/media/react.6ca9d736.svg"
+  },
+  {
+    "revision": "6f2ac16fe1468f3f23b550c58904d889",
+    "url": "/portfolio-v2/static/media/recipe-app.6f2ac16f.PNG"
+  },
+  {
+    "revision": "12d850aff3a46f18d138c429fc0e0ec9",
+    "url": "/portfolio-v2/static/media/resume.12d850af.pdf"
+  },
+  {
+    "revision": "4e3d1fbd00ec3a33fdb3ba6000705a7c",
+    "url": "/portfolio-v2/static/media/riddle.4e3d1fbd.PNG"
+  },
+  {
+    "revision": "a72c6ed1650eafe365deec90f84e80b0",
+    "url": "/portfolio-v2/static/media/sass.a72c6ed1.svg"
+  },
+  {
+    "revision": "7676401821f69f2fc160e1e80c9098d2",
+    "url": "/portfolio-v2/static/media/scratchlogo.76764018.png"
+  },
+  {
+    "revision": "33d912806a446b33cfac5d3b8c4ea848",
+    "url": "/portfolio-v2/static/media/skills.33d91280.svg"
+  },
+  {
+    "revision": "7a6b1cf2436309687d4ba9fc791d2270",
+    "url": "/portfolio-v2/static/media/sql.7a6b1cf2.svg"
+  },
+  {
+    "revision": "430f65cccd078a1cf170ce3dccf280bb",
+    "url": "/portfolio-v2/static/media/test.430f65cc.svg"
+  },
+  {
+    "revision": "4bf9f1d66887bdc4accda5e885610b8b",
+    "url": "/portfolio-v2/static/media/tools.4bf9f1d6.svg"
+  },
+  {
+    "revision": "86e0e8ecc3924f8c2a1f7e721d083c82",
+    "url": "/portfolio-v2/static/media/webdev.86e0e8ec.jpg"
+  }
+]);
